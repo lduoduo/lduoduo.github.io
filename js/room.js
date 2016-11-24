dc9bc09f4fcc9140a23099ca58323d78f7b54c31
Duo Duo <1025258687@qq.com>
2016-11-24 15:28:18 +0800

    34543
    34534

--- js/room.js
+++ js/room.js
@@ -45,103 +45,20 @@ var s = {
         FastClick.attach(document.body);
         sw.init();
     },
-    
-    //发送消息
-    sendMsg: function (msg) {
-        socket.send(my.info, msg);
-    },
-    //离开房间
-    leave: function () {
-        socket.emit('leave');
-    },
-    //join 房间
-    join: function () {
-        socket.emit('join', my.info);
-    },
+
     //初始化各种交互事件
     initEvent: function () {
-        // 发送消息
-        $('.J-msg-in').keydown(function (e) {
-            if (e.which === 13) {
-                e.preventDefault();
-                var msg = $(this).val();
-                if (msg == "") {
-                    return;
-                }
-                $(this).val('');
-//                 s.sendMsg(msg);
-            }
-        });
-
         $('body')
             //签到选择男女
-            .on('click', '.J-sex-select .radio', function (e) {
-                my.sex = $(e.target).val();
-                my.isReady = true;
-                my.info.sex = my.sex;
-                s.initSocket();
-                setTimeout(s.join, 0);
+            .on('click', '.J-open', function (e) {
+                push.open();
             })
             //发送聊天消息
-            .on('click', '.J-send-msg', function (e) {
-                var msg = $('.J-msg-in').val();
-                if (msg == "") {
-                    return;
-                }
-                $('.J-msg-in').val('');
-                s.sendMsg(msg);
+            .on('click', '.J-close', function (e) {
+                push.close();
             })
-    },
-    //定时显示签到的人
-    initTimer: function () {
-        my.timer = setInterval(function () {
-            if (my.addedlist.length > 0) {
-                var tmp = my.addedlist.shift();
-                my.list.push(tmp);
-                my[tmp.sex + 'list'].push(tmp);
-                Mt.alert({
-                    title: '',
-                    confirmBtnMsg: false,
-                    msg: '<div class="alert-img"><img src="/img/' + tmp.img + '"><p>' + tmp.id + '号进来了</p></div>',
-                    timer: 500,
-                    html: true
-                });
-            }
-        }, 2000);
-    },
-    //显示聊天信息
-    showMsg: function (user, msg) {
-        var className = "left";
-        if (user.id == my.info.id) {
-            className = "right";
-        }
-        console.log('msg from :' + JSON.stringify(user));
-        var message = "<li class='" + className + " item'><img src='/img/" + user.img + "'></img><span class='msg'>" + msg + "</span></li>";
-
-        $('.J-msg').append(message);
-        // 滚动条保持最下方
-        $('.J-msg').scrollTop($('.J-msg')[0].scrollHeight);
-
-        //弹幕提示-------------------------------
-        var colors = ['#ee2424', '#10b54b', '#de14d6', '#1476de'];
-        var v = Math.floor(Math.random() * 5) + 5;
-        var top = Math.floor(Math.random() * 50);
-        var color = colors[Math.floor(Math.random() * 4)];
-        var mydom = document.createElement('marquee');
-        $(mydom).attr('scrollamount', v).css({
-            'position': 'fixed',
-            'top': top + '%',
-            'z-index': 111,
-            'color': color
-        });
-        $(mydom).html(message);
-        // var html = "<marquee behavior=slide scrollamount=" + v + " style='position:fixed;top:"+top+"%;z-index:111;color:"+color+"'>"+message+"</marquee>";
-        $('.J-msg-align').append($(mydom));
-        setTimeout(function () {
-            console.log($(mydom));
-            $(mydom).remove();
-        }, 100000);
     }
+
 }
 
 sw = {
