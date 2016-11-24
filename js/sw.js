a1e4734134de7b24231db13214f54a5519e9dba6
Duo Duo <1025258687@qq.com>
2016-11-24 15:28:41 +0800

    22
    22

--- js/sw.js
+++ js/sw.js
@@ -1,1 +1,17 @@
-console.log('123');
+Notification.requestPermission();
+var flag = false;
+var timer = null;
+var push = {
+    open() {
+        timer = setInterval(function () {
+            new Notification("hello", {
+                silent: false,
+                body: "测试而已",
+                sound: "http://10.101.48.32:3002/media/fire.mp3"
+            })
+        }, 3000);
+    },
+    close() {
+        clearInterval(timer);
+    }
+}
