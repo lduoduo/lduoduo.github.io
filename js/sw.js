616e3e1ff9a0b993852eb8b5dc5d371df30470b7
Duo Duo <1025258687@qq.com>
2016-11-24 16:15:36 +0800

    33
    3

--- js/sw.js
+++ js/sw.js
@@ -1,6 +1,46 @@
-Notification.requestPermission();
-var flag = false;
-var timer = null;
+var port;
+
+// self.addEventListener('push', function (event) {
+//     var obj = event.data.json();
+
+//     if (obj.action === 'subscribe' || obj.action === 'unsubscribe') {
+//         fireNotification(obj, event);
+//         port.postMessage(obj);
+//     } else if (obj.action === 'init' || obj.action === 'chatMsg') {
+//         port.postMessage(obj);
+//     }
+// });
+
+// self.onmessage = function (e) {
+//     console.log(e);
+//     port = e.ports[0];
+// }
+
+// function fireNotification(obj, event) {
+//     var title = 'Subscription change';
+//     var body = obj.name + ' has ' + obj.action + 'd.';
+//     var icon = 'push-icon.png';
+//     var tag = 'push';
+
+//     event.waitUntil(self.registration.showNotification(title, {
+//         body: body,
+//         icon: icon,
+//         tag: tag
+//     }));
+// }
+
+
+
+
+
+// var CACHE_NAME = 'lduoduo-cache-v1';
+
+// // The files we want to cache
+// var urlsToCache = [
+//     '/',
+//     '/styles/main.css',
+//     '/script/main.js'
+// ];
 var push = {
     open() {
         timer = setInterval(function () {
@@ -15,3 +55,25 @@ var push = {
         clearInterval(timer);
     }
 }
+
+// // Set the callback for the install step
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    console.log(event);
+    Notification.requestPermission();
+    push.open();
+    // event.waitUntil(
+    //     caches.open(CACHE_NAME)
+    //         .then(function (cache) {
+    //             console.log('Opened cache');
+    //             return cache.addAll(urlsToCache);
+    //         })
+    // );
+});
+
+
+
+
+// var flag = false;
+// var timer = null;
+
