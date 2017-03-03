df83ca5573236314b2bda5f52dca0eb5bbb4a947
lduoduo <sdlina317@gmail.com>
2017-03-03 22:01:34 +0800

    add logic

--- gulp/src/home/home.js
+++ gulp/src/home/home.js
@@ -31,7 +31,7 @@
         },
         initWS: function () {
             if ('serviceWorker' in navigator) {
-                navigator.serviceWorker.register('js/sw.js', { scope: '/', insecure: true })
+                navigator.serviceWorker.register('js/sw.js', { scope: './', insecure: true })
                     .then(function (registration) {
                         // Registration was successful
                         console.log('ServiceWorker registration successful with scope: ', registration.scope);
@@ -43,7 +43,7 @@
         },
         initWW: function () {
             var _ = this;
-            _.ww = new Worker('ww.js');
+            _.ww = new Worker('js/ww.js');
             _.ww.addEventListener('message', function (e) {
                 console.log('get message from web workers');
                 console.log(e.data);
@@ -51,19 +51,19 @@
                 _.ww.terminate();
             });
 
-            _.ww.onerror(function (e) {
-                console.log('err : \n');
-                console.log(e);
-                //子线程调用完毕关闭
-                _.ww.terminate();
-            });
-
-            //或者这种写法
-            // _ww.addEventListener('error', function (event) {
-            //     console.log(event);
+            // _.ww.onerror(function (e) {
+            //     console.log('err : \n');
+            //     console.log(e);
             //     //子线程调用完毕关闭
             //     _.ww.terminate();
             // });
+
+            //或者这种写法
+            _ww.addEventListener('error', function (event) {
+                console.log(event);
+                //子线程调用完毕关闭
+                _.ww.terminate();
+            });
 
             setTimeout(function () {
                 console.log('post message to web workers');
